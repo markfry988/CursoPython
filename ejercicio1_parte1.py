8d6edc18ae102ff41e92ff0f22fec08e01ea5db8
markfry988 <92998939+markfry988@users.noreply.github.com>
2021-10-22 15:27:46 -0500

    Add files via upload

---
+++ ejercicio1_parte1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 12:14:30 2021
+
+@author: markf
+"""
+
+print("Empezando a trabajar con Python\n") #Muestra el enunciado, uso \n en todas las líneas para el salto de línea
+print("Realizado por: Marco Molina\n") #Muestra el creador
+print("Consultando los tipos de valores:\n") #Muestra la instruccción
+print("El tipo de dato de 875 es: \n") #Muestra la pregunta
+print(type(875),"\n") #Devuelve el tipo de dato solicitado
+print("El tipo de dato de 4.89 es: \n") #Muestra la pregunta
+print(type(4.89),"\n") #Devuelve el tipo de dato solicitado
+print("El tipo de dato de: Now is better than never. es: \n") #Muestra la pregunta
+print(type("Now is better than never."),"\n") #Devuelve el tipo de dato solicitado
+print("El tipo de dato de 1.32 es: \n") #Muestra la pregunta
+print(type(1.32),"\n") #Devuelve el tipo de dato solicitado
+print("¿El valor 5+8i corresponde a un valor entero? \n") #Muestra la pregunta
+print(isinstance(5+8j,int),"\n") #Compara con lo solicitado y devuelve el resultado
+print("¿El valor 8.2 corresponde a un valor entero? \n") #Muestra la pregunta
+print(isinstance(8.2,int),"\n") #Compara con lo solicitado y devuelve el resultado
+print("¿El texto: Readability counts. corresponde a un string? \n") #Muestra la pregunta
+print(isinstance("Readability counts.",str),"\n") #Compara con lo solicitado y devuelve el resultado
